0ffbe4bf7f9eaa6fb4b81d8b284ec2b2d8b3cb0f
Sithov Lorn <lornsithov@gmail.com>
2024-01-22 22:58:28 -0500

    use the product.js as the data

--- script/amazon.js
+++ script/amazon.js
@@ -1,32 +1,59 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceInCents: 1090
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceInCents: 2095
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceInCents: 799
-  }
-];
+// const products = [
+//   {
+//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//     rating: {
+//       stars: 4.5,
+//       count: 87
+//     },
+//     priceInCents: 1090
+//   },
+//   {
+//     image: "images/products/intermediate-composite-basketball.jpg",
+//     name: "Intermediate Size Basketball",
+//     rating: {
+//       stars: 4,
+//       count: 127
+//     },
+//     priceInCents: 2095
+//   },
+//   {
+//     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//     name: "Adults Plain Cotton T-Shirt - 2 Pack",
+//     rating: {
+//       stars: 4.5,
+//       count: 56
+//     },
+//     priceInCents: 799
+//   },
+//   {
+//     image: "images/products/black-2-slot-toaster.jpg",
+//     name: "2 Slot Toaster - Black",
+//     rating: {
+//       stars: 5,
+//       count: 2197
+//     },
+//     priceInCents: 1899
+//   },
+//   {
+//     image: "images/products/6-piece-white-dinner-plate-set.jpg",
+//     name: "6 Piece White Dinner Plate Set",
+//     rating: {
+//       stars: 4,
+//       count: 37
+//     },
+//     priceInCents: 2067
+//   },
+//   {
+//     image: "images/products/6-piece-non-stick-baking-set.webp",
+//     name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
+//     rating: {
+//       stars: 4.5,
+//       count: 175
+//     },
+//     priceInCents: 3499
+//   }
+// ];
 
 let productHTML = " "; 
 // every time a product is created, it will be stored in this STRING
@@ -86,6 +113,5 @@ products.forEach((product) => {
   `
 });
 
-console.log(productHTML)
-
+// the DOM element for the HTML replacement
 document.querySelector('.js-product-grid').innerHTML = productHTML;
