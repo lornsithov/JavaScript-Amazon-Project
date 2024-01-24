47a125d07256049b68dcd17d6e165a0549e8aac4
Sithov Lorn <lornsithov@gmail.com>
2024-01-24 01:10:14 -0500

    Generate HTML for the order summary which is from checkout.html to checkout.js

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,15 @@
 // any products will be included in this cart 
 // when the user clicks add-to-cart button
-export const cart = [];
+export const cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  }
+];
 // step 2: export cart (using the module's export)
 
 // add-to-cart FUNCTION
