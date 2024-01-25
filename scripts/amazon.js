d1ab72cfa4bb043f48665da533e7eb293d9aa220
Sithov Lorn <lornsithov@gmail.com>
2024-01-25 01:37:45 -0500

    added message when clicking add to cart button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -107,7 +107,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart" data-product-id="${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -134,25 +134,68 @@ function calculateAndUpdateTotalQuantity() {
   document.querySelector('.cart-quantity').innerHTML = cartQuantity;
 }
 
+// Added Message function
+// const addedMessageFunc = document.addEventListener('DOMContentLoaded', function(){
+//   const addedMessage = document.querySelectorAll('.js-added-to-cart');
+//   addedMessage.forEach((message) => {
+//     const messageId = message.dataset.productId;
+//     console.log(messageId)
+//     messageId.forEach()
+//   })
+// });
+  
+// we use DOMContentLoaded since the function is splitted into two parts plus ensuring that it will work with any broswers
+
 // the DOM element for the buttons inside each product
 // data attributes v dataset
-// MAIN function of the BUTTONS
-document.querySelectorAll('.js-add-to-card').forEach((button)=>{
+// MAIN function of the ADD-TO-CART BUTTONS
+document.querySelectorAll('.js-add-to-card').forEach((button) => {
+  // let timeoutId; // Variable to store timeout ID (1st appear and disappear)
   // dataset property basically 
   // gives us all the data attributes 
-  // that attached to the button
+  // that are attached to the button
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
-    // since "productName" is inside "dataset", 
-		// then we can use "button.dataset.productName" to set with all the buttons
+    // since "productId" is inside "dataset", 
+    // then we can use "button.dataset.productId" to get all the id
     
-    // calling add-to-cart func
+    // added-message func starts
+    const addedMessage = document.querySelector(`.js-added-to-cart[data-product-id="${productId}"]`);
+    // 1st (appear & disappear)
+    // Clear any previously scheduled timeout
+    // clearTimeout(timeoutId);
+    // if (addedMessage) {
+    //   // Change CSS properties
+    //   addedMessage.style.opacity = '1';
+
+    //   // Apply a delay using setTimeout to make the element disappear after 2 seconds
+    //   timeoutId = setTimeout(() => {
+    //     addedMessage.style.opacity = '0';
+        
+    //     // Set a second timeout to hide the element
+    //     setTimeout(() => {
+    //       addedMessage.style.display = 'block';
+    //     }, 500); // Adjust the disappearance time (in milliseconds) as needed
+    //   }, 2000); // Show the style for 2 seconds (2000 milliseconds)
+    // }
+    // 2nd (appear & disappear)
+    addedMessage.classList.add('added-to-cart-visible');
+    setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+    // Save the timeoutId for this product
+    // so we can stop it later if we need to.
+    addedMessageTimeouts[productId] = timeoutId;
+    // added-message func ends
+    
     addToCart(productId);
-    
     // calling add-to-cart func
+    
     calculateAndUpdateTotalQuantity();
-  })
+    // calling add-to-cart func
+  });
 });
 // forEach to loop through each of the add-to-cart buttons
 // button parameter inside forEach is for those buttons that should added to the parameter when they are clicked
 // now add the addEventListener using the Click function
+
