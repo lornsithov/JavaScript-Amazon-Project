faac335004803fb874484143b26875f8c1a7036e
Sithov Lorn <lornsithov@gmail.com>
2024-02-01 15:13:12 -0500

    generate order summary html

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -199,45 +199,6 @@ export function renderOrderSummary() {
       // rerun the code since it needs to be refreshed
     });
   });
-
-  // Generate order summary HTML
-  let orderSummaryHTML = 
-  `
-    <div class="payment-summary-title">
-      Order Summary
-    </div>
-
-    <div class="payment-summary-row">
-      <div>Items (3):</div>
-      <div class="payment-summary-money">$42.75</div>
-    </div>
-
-    <div class="payment-summary-row">
-      <div>Shipping &amp; handling:</div>
-      <div class="payment-summary-money">$4.99</div>
-    </div>
-
-    <div class="payment-summary-row subtotal-row">
-      <div>Total before tax:</div>
-      <div class="payment-summary-money">$47.74</div>
-    </div>
-
-    <div class="payment-summary-row">
-      <div>Estimated tax (10%):</div>
-      <div class="payment-summary-money">$4.77</div>
-    </div>
-
-    <div class="payment-summary-row total-row">
-      <div>Order total:</div>
-      <div class="payment-summary-money">$52.51</div>
-    </div>
-
-    <button class="place-order-button button-primary">
-      Place your order
-    </button>
-    </div>
-  `;
-  document.querySelector('.js-payment-summary').innerHTML = orderSummaryHTML;
 }
 
 // renderOrderSummary();
